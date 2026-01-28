65fb86e52393bd4c41c246c5844cefe3d1086f21
luishdezc <luisg.hernandez@iteso.mx>
2026-01-28 12:04:00 -0600

    chore: initial project structure

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,3 @@
+## Checklist
+
+- [ ] Submitted code is covered by tests
---
+++ hello_world.py
@@ -0,0 +1,10 @@
+"""Hello world"""
+
+
+def hello_world():
+    """Imprime un mensaje"""
+    print("Hola mundo")
+
+
+if __name__ == "__main__":
+    hello_world()
